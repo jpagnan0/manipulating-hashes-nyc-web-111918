efe63079065512baf4cf1f77a92aa4581ab6ee85
Adam Saland <a.jpagnano@gmail.com>
2018-11-10 23:31:11 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Fredd Mercury"][:favorite_icecream_flavors]
+  contacts["Fredd Mercury"][:favorite_icecream_flavors].delete("strawberry")
   #remember to return your newly altered contacts hash!
   contacts
 end
